1ff113120830096d089b7360d0465c335b1525ee
webdev03 <75148774+webdev03@users.noreply.github.com>
2021-05-12 20:08:48 +1200

    Fix up a few things

--- main.js
+++ main.js
@@ -5,17 +5,19 @@ var ms, mspercent, questionsShown = 0;
 
 
 
+function startQuickMaths() {
+    document.getElementById('progress-element').style.visibility = "visible"
+    start = Date.now();
+    var countInterval = setInterval(function () {
+        var delta = Date.now() - start;
+        ms = delta / 1000
+        // console.log(ms)
+        mspercent = ms / seconds * 100
+        if (delta / 1000 > seconds - 0.03) {
+            start = Date.now();
+        } else {
+            document.getElementById("timer").style.width = 100 - mspercent.toString() + "%";
+        }
+    }, 10);
+}
 
-document.getElementById("progress-element").style.visibility = "visible"
-var countInterval = setInterval(function() {
-    var delta = Date.now() - start; 
-    ms = delta / 1000
-    // console.log(ms)
-    mspercent = ms / seconds * 100
-    console.log(mspercent)
-    if (delta / 1000 > seconds - 0.03) {
-        start = Date.now();
-    } else {
-        document.getElementById("timer").style.width = 100-mspercent.toString() + "%"
-    }
-}, 10);
